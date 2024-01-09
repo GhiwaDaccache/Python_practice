541517df08aae7bf6cc538dc038191f5585d8e3e
GhiwaDaccache <ghiwadaccache4@gmail.com>
2024-01-09 15:16:25 +0200

    panagram

--- Exercises.py
+++ Exercises.py
@@ -20,6 +20,7 @@ def to_camel_case(text):
         
     return "".join(result)
 
+
 ############
 # Exercice 2
 ############
@@ -36,3 +37,19 @@ def disemvowel(string_):
     string_ = "".join(new_string)
     return string_
 
+############
+# Exercice 3
+############
+
+# A pangram is a sentence that contains every single letter of the alphabet at least once. 
+# For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram.
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. 
+# Ignore numbers and punctuation.
+
+def is_pangram(s):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    s1 = s.lower()
+    for letter in alphabet:
+        if letter not in s1:      
+            return False
+    return True
